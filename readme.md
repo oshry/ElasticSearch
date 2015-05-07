cedf688ac811b3bef8d39f46dff51aeb4590ca3b
oshry <oshrylevy@gmail.com>
2015-05-07 17:15:30 +0300

    Add ReadMe

---
+++ readme.md
@@ -0,0 +1,4 @@
+Elastic Search Implementation
+=============================
+ - FrontEnd:[app/] Get Ajax Json Response From src
+ - BackEnd:[src/] All Request Move Into [src/http] For Delivery
--- src/http/index.php
+++ src/http/index.php
@@ -12,21 +12,13 @@ set_error_handler([ 'Search\classes\Exception', 'error_handler' ]);
 register_shutdown_function([ 'Search\classes\Exception', 'shutdown_handler' ]);
 
 $config = include APPPATH.'config/database.php';
-//echo "<pre>",print_r($config),"</pre>";
 $db = Search\Repository\DataRepository::instance($config, 'dev');
-//$app = new App\Manager($config);
-//echo "<pre>",print_r($db),"</pre>";die();
 
 // Load app-specific routes
 $routes = include APPPATH.'config/routes.php';
-//echo "<pre>",print_r($routes),"</pre>";
-//var_dump($app);
-//die('sdsd');
 echo (new Search\Delivery\Request([
 		'app' => $db,
 		'base_url' => '/Search/src/http',
 		'routes' => $routes
 	]))
 	->execute();
-	//->body();
-//Question 1: how execute() to request and body() to response
